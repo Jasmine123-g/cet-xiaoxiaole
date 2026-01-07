5f3f29de63eece85a667dbfc2b1fd3d67e5df872
Jasmine123-g <p55j54qk46@privaterelay.appleid.com>
2026-01-07 18:23:05 +0800

    Add files via upload

---
+++ CET4_levels.js
@@ -0,0 +1,38 @@
+window.CET4_LEVELS = {
+  "level1": [
+    {
+      "en": "book",
+      "cn": "书"
+    },
+    {
+      "en": "study",
+      "cn": "学习"
+    },
+    {
+      "en": "school",
+      "cn": "学校"
+    },
+    {
+      "en": "teacher",
+      "cn": "老师"
+    }
+  ],
+  "level2": [
+    {
+      "en": "abandon",
+      "cn": "放弃"
+    },
+    {
+      "en": "ability",
+      "cn": "能力"
+    },
+    {
+      "en": "absorb",
+      "cn": "吸收"
+    },
+    {
+      "en": "academic",
+      "cn": "学术的"
+    }
+  ]
+}
---
+++ CET6_levels.js
@@ -0,0 +1,38 @@
+window.CET6_LEVELS = {
+  "level1": [
+    {
+      "en": "abstract",
+      "cn": "抽象的"
+    },
+    {
+      "en": "advocate",
+      "cn": "提倡"
+    },
+    {
+      "en": "allocate",
+      "cn": "分配"
+    },
+    {
+      "en": "analogy",
+      "cn": "类比"
+    }
+  ],
+  "level2": [
+    {
+      "en": "ambiguous",
+      "cn": "模糊的"
+    },
+    {
+      "en": "anticipate",
+      "cn": "预期"
+    },
+    {
+      "en": "approximate",
+      "cn": "大约的"
+    },
+    {
+      "en": "assumption",
+      "cn": "假设"
+    }
+  ]
+}
